16c132ce77c56145ae35a6c14586638b0942bb26
zorro901 <zoro907@gmail.com>
2021-09-06 21:31:16 +0900

    function components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function App() {
-  return (
+const App =()=> {
+    return (
+      <>
     <input type="text" onChange={()=>console.log("clicked")}/>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+      </>
   )
 }
+
+const Cat = () => <div>にゃー</div>
 
 export default App;
